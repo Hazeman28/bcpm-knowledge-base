2a5b5566d93074727e9b3b2f81f2968254967122
Nizomiddin Toshpulatov <niza@outlook.sk>
2021-03-18 07:54:55 +0000

    KB-0 Add deploy job

---
+++ .space.kts
@@ -0,0 +1,22 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Deploy to GitHub pages") {
+    container(displayName = "Deploy", image = "ubuntu") {
+    	shellScript {
+        	interpreter = "/bin/bash"
+            location = "./deploy.sh"
+        }
+        
+        stratOn {
+        	gitPush {
+            	branchFilter {
+                	"refs/heads/release"
+                }
+            }
+        }
+    }
+}
